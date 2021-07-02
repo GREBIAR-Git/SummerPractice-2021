afec12314ece17340a7c1fef707b21cd89bad092
GREBIAR-Git <nikitos.muzalevskiY@mail.ru>
2021-07-03 02:33:57 +0300

    fix

--- Charts/TranslatingExpression.cs
+++ Charts/TranslatingExpression.cs
@@ -6,120 +6,127 @@ namespace Charts
     {
         public static float Translating(string str,float x)
         {
-            str = str.ToLower();
-            str = str.Replace(" ", "");
-            str = str.Replace("--", "-");
-            str = str.Replace("x", x.ToString());
-            for (int i = 0; str.Length > i; i++)
+            try
             {
-                if (str.Length-3 > i && str[i] == 's' && str[i + 1] == 'i' && str[i + 2] == 'n')
+                str = str.ToLower();
+                str = str.Replace(" ", "");
+                str = str.Replace("--", "-");
+                str = str.Replace("x", x.ToString());
+                for (int i = 0; str.Length > i; i++)
                 {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    if (funcO(3, ref i, ref str, ref inParentheses,ref placeInsertion))
+                    if (str.Length - 3 > i && str[i] == 's' && str[i + 1] == 'i' && str[i + 2] == 'n')
                     {
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Sin(Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
-                    }
-                }
-                else if (str.Length - 3 > i && str[i] == 'c' && str[i + 1] == 'o' && str[i + 2] == 's')
-                {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    if (funcO(3, ref i, ref str, ref inParentheses, ref placeInsertion))
-                    {
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Cos(Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
-                    }
-                }
-                else if (str.Length - 3 > i && str[i] == 'l' && str[i + 1] == 'o' && str[i + 2] == 'g')
-                {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    if (funcO(3, ref i, ref str, ref inParentheses, ref placeInsertion))
-                    {
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Log(Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
-                    }
-                }
-                else if (str.Length - 2 > i && str[i] == 't' && str[i + 1] == 'g')
-                {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    if (funcO(2, ref i, ref str, ref inParentheses, ref placeInsertion))
-                    {
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Tan(Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
-                    }
-                }
-                else if (str.Length - 2 > i && str[i] == 'l' && str[i + 1] == 'n')
-                {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    if (funcO(2, ref i, ref str, ref inParentheses, ref placeInsertion))
-                    {
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Log10(Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
-                    }
-                }
-                else if (str.Length - 1 > i&&str[i] == '^')
-                {
-                    string inParentheses = string.Empty;
-                    int placeInsertion = 0;
-                    int countS=0;
-                    int f;
-                    for (f=i - 1; f>=0;f--)
-                    {
-                        if (Char.IsDigit(str[f]))
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        if (funcO(3, ref i, ref str, ref inParentheses, ref placeInsertion))
                         {
-                            countS++;
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Sin(Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
                         }
-                        else
+                    }
+                    else if (str.Length - 3 > i && str[i] == 'c' && str[i + 1] == 'o' && str[i + 2] == 's')
+                    {
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        if (funcO(3, ref i, ref str, ref inParentheses, ref placeInsertion))
                         {
-                            if(str[f]=='-')
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Cos(Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
+                        }
+                    }
+                    else if (str.Length - 3 > i && str[i] == 'l' && str[i + 1] == 'o' && str[i + 2] == 'g')
+                    {
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        if (funcO(3, ref i, ref str, ref inParentheses, ref placeInsertion))
+                        {
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Log(Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
+                        }
+                    }
+                    else if (str.Length - 2 > i && str[i] == 't' && str[i + 1] == 'g')
+                    {
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        if (funcO(2, ref i, ref str, ref inParentheses, ref placeInsertion))
+                        {
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Tan(Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
+                        }
+                    }
+                    else if (str.Length - 2 > i && str[i] == 'l' && str[i + 1] == 'n')
+                    {
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        if (funcO(2, ref i, ref str, ref inParentheses, ref placeInsertion))
+                        {
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Log10(Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
+                        }
+                    }
+                    else if (str.Length - 1 > i && str[i] == '^')
+                    {
+                        string inParentheses = string.Empty;
+                        int placeInsertion = 0;
+                        int countS = 0;
+                        int f;
+                        for (f = i - 1; f >= 0; f--)
+                        {
+                            if (Char.IsDigit(str[f]))
                             {
                                 countS++;
-
                             }
                             else
                             {
-                                f++;
+                                if (str[f] == '-')
+                                {
+                                    countS++;
+
+                                }
+                                else
+                                {
+                                    f++;
+                                }
+                                break;
                             }
-                            break;
                         }
-                    }
-                    string byff = "";
-                    if(f==-1)
-                    {
-                        f = 0;
-                    }
-                    byff = str.Substring(f, countS);
-                    placeInsertion = f;
-                    if (funcR(1, ref i, ref str, ref inParentheses, ref placeInsertion))
-                    {
-                        if (byff[0] == '-' && byff.Length == 1)
+                        string byff = "";
+                        if (f == -1)
                         {
-                            byff = "-1";
+                            f = 0;
                         }
-                        str = str.Replace(',', '.');
-                        string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
-                        str = str.Insert(placeInsertion, Math.Round(Math.Pow(double.Parse(byff),Convert.ToDouble(t)), 1).ToString());
-                        i = -1;
+                        byff = str.Substring(f, countS);
+                        placeInsertion = f;
+                        if (funcR(1, ref i, ref str, ref inParentheses, ref placeInsertion))
+                        {
+                            if (byff[0] == '-' && byff.Length == 1)
+                            {
+                                byff = "-1";
+                            }
+                            str = str.Replace(',', '.');
+                            string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
+                            str = str.Insert(placeInsertion, Math.Round(Math.Pow(double.Parse(byff), Convert.ToDouble(t)), 1).ToString());
+                            i = -1;
+                        }
                     }
                 }
+                str = str.Replace(',', '.');
+                str = (new System.Data.DataTable()).Compute(str, "").ToString();
+                if (str == "")
+                {
+                    return 0;
+                }
+                return float.Parse(str) * -1;
             }
-            str = str.Replace(',', '.');
-            str = (new System.Data.DataTable()).Compute(str, "").ToString();
-            if(str=="")
+            catch
             {
                 return 0;
             }
-            return float.Parse(str)*-1;
         }
 
         static bool funcO(int num, ref int i, ref string str, ref string inParentheses, ref int tyt)
