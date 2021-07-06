13709e60f424913e3a56aaa9b87c6a77333b2a72
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-07-06 05:24:24 +0300

    +теперь можно вводить функцию без скобок(sin(x) == sinx; x^(2) == x^2) fix

--- Charts/TranslatingExpression.cs
+++ Charts/TranslatingExpression.cs
@@ -4,6 +4,24 @@ namespace Charts
 {
     class TranslatingExpression
     {
+        public static void addBracket(ref string str,int i,int offset)
+        {
+            if (str[i + offset] != '(')
+            {
+                int f = i + offset;
+                char d = str[f];
+                if(str[f]=='-')
+                {
+                    f++;
+                }
+                while (str.Length > f &&( Char.IsDigit(str[f]) || str[f]=='x'))
+                {
+                    f++;
+                }
+                str = str.Insert(i + offset, "(");
+                str = str.Insert(f + 1, ")");
+            }
+        }
         public static float Translating(string str,float x)
         {
             try
@@ -11,6 +29,52 @@ namespace Charts
                 str = str.ToLower();
                 str = str.Replace(" ", "");
                 str = str.Replace("--", "-");
+                str = str.Replace(",", ".");
+                for(int i=0;i<str.Length;i++)
+                {
+                    if (str.Length - 1 > i&& str[i] == '^')
+                    {
+                        addBracket(ref str, i, 1);
+                        if (str[i - 1] != ')')
+                        {
+                            int f = i - 1;
+                            char d = str[f];
+                            while (f > 0 && Char.IsDigit(str[f]))
+                            {
+                                f--;
+                            }
+                            str = str.Insert(i, ")");
+                            if(str[f]=='-')
+                            {
+                                str = str.Insert(f + 1, "(");
+                            }
+                            else
+                            {
+                                str = str.Insert(f, "(");
+                            }
+                        }
+                    }
+                    else if(str.Length - 2 > i && str[i] == 'l' && str[i+1] == 'n')
+                    {
+                        addBracket(ref str, i, 2);
+                    }
+                    else if (str.Length - 2 > i && str[i] == 't' && str[i+1] == 'g')
+                    {
+                        addBracket(ref str, i, 2);
+                    }
+                    else if (str.Length - 3 > i && str[i] == 's' && str[i+1] == 'i' && str[i + 2] == 'n')
+                    {
+                        addBracket(ref str, i, 3);
+                    }
+                    else if (str.Length - 3 > i && str[i] == 'c' && str[i+1] == 'o' && str[i + 2] == 's')
+                    {
+                        addBracket(ref str, i, 3);
+                    }
+                    else if (str.Length - 3 > i && str[i] == 'l' && str[i+1] == 'o' && str[i + 2] == 'g')
+                    {
+                        addBracket(ref str, i, 3);
+                    }
+                }
                 str = str.Replace("x", x.ToString());
                 for (int i = 0; str.Length > i; i++)
                 {
@@ -74,39 +138,18 @@ namespace Charts
                         string inParentheses = string.Empty;
                         int placeInsertion = 0;
                         int countS = 0;
-                        int f;
-                        for (f = i - 1; f >= 0; f--)
+                        int f=i-2;
+                        while(str[f]!='(')
                         {
-                            if (Char.IsDigit(str[f]))
-                            {
-                                countS++;
-                            }
-                            else
-                            {
-                                if (str[f] == '-')
-                                {
-                                    countS++;
-                                }
-                                else
-                                {
-                                    f++;
-                                }
-                                break;
-                            }
+                            countS++;
+                            f--;
                         }
+                        f++;
                         string byff = "";
-                        if (f == -1)
-                        {
-                            f = 0;
-                        }
                         byff = str.Substring(f, countS);
                         placeInsertion = f;
                         if (funcR(1, ref i, ref str, ref inParentheses, ref placeInsertion))
                         {
-                            if (byff[0] == '-' && byff.Length == 1)
-                            {
-                                byff = "-1";
-                            }
                             str = str.Replace(',', '.');
                             string t = (new System.Data.DataTable()).Compute(inParentheses, "").ToString();
                             str = str.Insert(placeInsertion, Math.Round(Math.Pow(double.Parse(byff), Convert.ToDouble(t)), 2).ToString());
@@ -120,7 +163,7 @@ namespace Charts
                 {
                     return 0;
                 }
-                return float.Parse(str) * -1;
+                return -float.Parse(str);
             }
             catch
             {
@@ -162,6 +205,7 @@ namespace Charts
             }
             if (Search(inParentheses) == 0)
             {
+                str = str.Insert(i + 1, ")");
                 str = str.Substring(0, tyt) + str.Substring(i + 1, str.Length - i - 1);
                 return true;
             }
